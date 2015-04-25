00bc5a233ad75aa3748776ee3fc04b2a8ce83184
iuqrul <iuqrul@gmail.com>
2015-04-25 16:14:02 +0200

    Carga inicial de proyectos del curso

---
+++ MisLugaresJava/mislugares/src/main/java/com/example/mislugares/Principal.java
@@ -0,0 +1,16 @@
+package com.example.mislugares;
+
+/**
+ * Created by Javi on 09/04/2015.
+ */
+public class Principal {
+
+    public static void main(String[] main) {
+        Lugar lugar = new Lugar("Escuela Politécnica Superior de Gandía",
+                "C/ Paranimf, 1 46730 Gandia (SPAIN)", -0.166093, 38.995656,
+                TipoLugar.EDUCACION, 962849300, "http://www.epsg.upv.es",
+                "Uno de los mejores lugares para formarse.", 3);
+        System.out.println("Lugar " + lugar.toString());
+    }
+
+}
